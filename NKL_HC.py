b75bba2157262d8e5a30442f1f14c175bd59154e
Salim_AMRI <amri.salim.2013@gmail.com>
2023-09-20 16:29:44 +0200

    recuperation de données

--- NKL_HC.py
+++ NKL_HC.py
@@ -31,22 +31,15 @@ args = parser.parse_args()
 
 # Paramètres initiaux en fonction des arguments
 type_strategy = args.type_strategy
-
 N = args.N
 K = args.K
 alpha = args.alpha
-
-
-
 
 train_path = "./benchmark/N_" + str(N) + "_K_" + str(K) + "/train/"
 valid_path = "./benchmark/N_" + str(N) + "_K_" + str(K) + "/validation/"
 nb_restarts = args.nb_restarts
 nb_instances = args.nb_instances
-
 nb_jobs = nb_restarts*nb_instances
-
-
 sigma_init = args.sigma_init
 max_generations = args.max_generations
 
@@ -56,18 +49,13 @@ f = open(pathResult + nameResult, "w")
 f.write("generation,avg_training_score,avg_validation_score\n")
 f.close()
 
-
 def get_Score_trajectory(type_strategy, N, K, network, path, nb_intances, idx_run, alpha=None):
 
     i = idx_run%nb_intances
-
-
     name_instance = path + "nk_" + str(N) + "_" + str(K) + "_" + str(i) + ".txt"
 
     #print("launch instance " + name_instance)
-
     env = EnvNKlandscape(name_instance)
-
     env.reset()
     terminated = False
     current_score = env.score()
@@ -92,28 +80,20 @@ def get_Score_trajectory(type_strategy, N, K, network, path, nb_intances, idx_ru
             action_id = non_ordered_neigh.index(neigh[0][action])
 
         if type_strategy == "NN_withTabu":
-
-            # Stratégie basée sur le réseau neuronal (NN)
+            # Stratégie basée sur le réseau neuronal (NN with Tabu)
             non_ordered_neigh = copy.deepcopy(neigh)
             tabuList = env.getTabuList()
-
             df = pd.DataFrame()
             df["fitness"] = neigh
             df["tabulist"] = tabuList
-
             df = df.sort_values(by="fitness")
-
             sorted_neighbors = df["fitness"].values
             sorted_tabuList = df["tabulist"].values
-
             stacked_input = np.hstack((sorted_neighbors, sorted_tabuList))
             stacked_input = torch.tensor(stacked_input, dtype=torch.float32).unsqueeze(0)
-
             out = network(stacked_input.unsqueeze(0)).squeeze(0)
             action = out.argmax().item()
-
             action_id = non_ordered_neigh.index(sorted_neighbors[action])
-
 
         elif type_strategy == "hillClimber":
             # Stratégie HillClimber
@@ -129,30 +109,19 @@ def get_Score_trajectory(type_strategy, N, K, network, path, nb_intances, idx_ru
         elif type_strategy == "tabu":
             # Stratégie Tabu
             tabuList = env.getTabuList()
-
-
             low_values = np.ones(tabuList.shape) * float("-inf")
             filter_neigh = np.where(tabuList < 1, neigh, low_values )
-
             action_id = int(np.argmax(np.array(filter_neigh)))
 
             ### Critere aspiration
-
             # best_delta = -99999
             # action_id = -1
-            #
             # for i in range(N):
-            #
             #     delta = neigh[i]
-            #
             #     if(tabuList[i] == 0 or delta + current_score > bestScore):
-            #
             #         if(delta > best_delta):
             #             best_delta = delta
             #             action_id = i
-
-
-
 
         if action_id >= 0:
             state, deltaScore, terminated = env.step(action_id)
@@ -167,7 +136,6 @@ def get_Score_trajectory(type_strategy, N, K, network, path, nb_intances, idx_ru
             bestScore = current_score
 
     return bestScore
-
 
 # Fonction pour calculer le score moyen d'une stratégie
 def get_average_score_strategy(type_strategy, N, K, weights, network, path, nb_instances, nb_restarts, nb_jobs, alpha=None):
@@ -186,8 +154,6 @@ def get_average_score_strategy(type_strategy, N, K, weights, network, path, nb_i
         average_score += score
 
     return average_score / (nb_instances * nb_restarts)
-
-
 
 # Fonction d'évaluation pour CMA-ES
 def evaluate_weights_NN(type_strategy, N, K, solution, network, path, nb_instances, nb_restarts, nb_jobs, alpha=None):
@@ -208,21 +174,13 @@ def evaluate_weights_NN(type_strategy, N, K, solution, network, path, nb_instanc
 
     # Évaluez le réseau de neurones avec les nouveaux poids
     average_score = get_average_score_strategy(type_strategy, N, K, weights, network, path, nb_instances, nb_restarts,nb_jobs, alpha)
-
-
-
     return average_score
 
-
-
 ## Add save result
-
-if(type_strategy == "NN_withTabu" or type_strategy =="NN"):
-
+if type_strategy == "NN_withTabu" or type_strategy =="NN":
     # Création de l'architecture du réseau de neurones
-
     if(type_strategy == "NN_withTabu"):
-        layers_size = [2*N, 2 * N, N]
+        layers_size = [2 * N, 2 * N, N]
     else:
         layers_size = [N, 2 * N, N]
 
@@ -243,7 +201,6 @@ if(type_strategy == "NN_withTabu" or type_strategy =="NN"):
 
     print("Taille de la population dans CMA-ES :", es.popsize)
 
-
     # Création d'une liste de réseaux de neurones pour chaque membre de la population CMA-ES
     list_nnet = [Net(layers_size) for i in range(es.popsize)]
 
@@ -258,15 +215,12 @@ if(type_strategy == "NN_withTabu" or type_strategy =="NN"):
         solutions = es.ask()  # Échantillonnez de nouveaux vecteurs de poids
 
         # Évaluez les performances de chaque solution en parallèle
-
-
         training_scores = []
         for idx, solution in enumerate(solutions):
             print("Eval individu " + str(idx))
             training_scores.append(evaluate_weights_NN(type_strategy, N, K, solution, list_nnet[idx], train_path, nb_instances, nb_restarts, nb_jobs, alpha))
-
-
         best_current_score = float("-inf")
+
         # Mettez à jour la meilleure solution trouvée par CMA-ES
         for idx, score in enumerate(training_scores):
             if score > best_current_score:
@@ -284,20 +238,42 @@ if(type_strategy == "NN_withTabu" or type_strategy =="NN"):
         f.write(str(generation) + "," + str(max(training_scores)) + "," + str(validation_score) + "\n")
         f.close()
 
-
         if(validation_score > best_global_validation_score):
             best_global_validation_score = validation_score
             np.savetxt("solutions/best_solution.csv" , best_current_solution)
-
 
         # Mettez à jour la barre de progression
         pbar.set_postfix(avg_training_score=max(training_scores), avg_validation_score=validation_score)
         pbar.update(1)  # Incrémentation de la barre de progression
 
 else:
-
     print("Évaluation de la stratégie " + type_strategy)
-    average_score_baseline = get_average_score_strategy(type_strategy, N, K, None, None, valid_path,
-                                                           nb_instances, nb_restarts, nb_jobs)
+    average_score_baseline = get_average_score_strategy(type_strategy, N, K, None, None, valid_path, nb_instances, nb_restarts, nb_jobs)
     print("Score moyen de la stratégie " + type_strategy + " sur l'ensemble de validation :")
     print(average_score_baseline)
+
+
+# Ajoutez un préfixe au nom du fichier de sortie en fonction de la stratégie
+if type_strategy == "NN":
+    strategy_prefix = "NN"
+elif type_strategy == "NN_withTabu":
+    strategy_prefix = "NN_withTabu"
+elif type_strategy == "hillClimber":
+    strategy_prefix = "hillClimber"
+elif type_strategy == "IteratedhillClimber":
+    strategy_prefix = "IteratedhillClimber"
+elif type_strategy == "tabu":
+    strategy_prefix = "tabu"
+else:
+    strategy_prefix = "unknown"
+
+# Créez un nom de fichier de sortie unique pour chaque stratégie
+nameResult = "test_" + strategy_prefix + "_N_" + str(N) + "_K_" + str(K) + "_" + str(datetime.date) + ".txt"
+f = open(pathResult + nameResult, "w")
+f.write("generation,avg_training_score,avg_validation_score\n")
+f.close()
+
+# Enregistrez les résultats dans le fichier de sortie approprié
+f = open(pathResult + nameResult, "a")
+f.write(str(generation) + "," + str(max(training_scores)) + "," + str(validation_score) + "\n")
+f.close()
